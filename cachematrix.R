58e8b4aab001c593e6bfd38a877c257a4bb9a90d
Ivan--M <mihajlovich.ivan@gmail.com>
2014-10-25 16:12:53 +0400

    finctions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+
+## Cashing functions
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInv <- function(mean) m <<- mean
+  getInv <- function() m
+  list(set = set, get = get,
+       setInv = setInv,
+       getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## Actual cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getInv()#check computed matrix
+  
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m) #show if already has been computed
+  }
+
+
+  x$setInv(solve(x$get())) #set cache
+  
+  x$getInv() #show result
 }
